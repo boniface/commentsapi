1cc2cb347fe33398265633be7a18836802d2923b
tebogokgopa214031179 <tebogokgopa84@gmail.com>
2016-10-29 14:19:08 +0200

    sites repos done

---
+++ app/repositories/util/KeysRepository.scala
@@ -0,0 +1,8 @@
+package repositories.util
+
+/**
+  * Created by Quest on 2016/10/22.
+  */
+class KeysRepository {
+
+}
---
+++ app/services/sites/AdminStatusService.scala
@@ -0,0 +1,8 @@
+package services.sites
+
+/**
+  * Created by Quest on 2016/10/29.
+  */
+trait AdminStatusService {
+
+}
