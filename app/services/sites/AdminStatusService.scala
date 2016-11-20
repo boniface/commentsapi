b9594cd131b5d0088fed88f3e1b3f2311bd56dd6
tebogokgopa214031179 <tebogokgopa84@gmail.com>
2016-11-20 07:57:39 +0200

    conflict resolution 1

--- app/services/sites/AdminStatusService.scala
+++ app/services/sites/AdminStatusService.scala
@@ -1,8 +1,22 @@
 package services.sites
 
+import com.websudos.phantom.dsl._
+import domain.sites.AdminStatus
+import services.Service
+import services.sites.impl.AdminStatusServiceImpl
+
+import scala.concurrent.Future
+
 /**
   * Created by Quest on 2016/10/29.
   */
 trait AdminStatusService {
-
+  def save(adminStatus: AdminStatus):Future[ResultSet]
+  def getAdminStatusById(adminStatusId:String):Future[Option[AdminStatus]]
+  def deleteById(adminStatusId: String):Future[ResultSet]
+  def getAllAdminStatus: Future[Seq[AdminStatus]]
 }
+object AdminStatusService extends Service{
+  def apply():AdminStatusService = new AdminStatusServiceImpl
+}
+
---
+++ app/services/sites/SitesService.scala
@@ -0,0 +1,21 @@
+package services.sites
+
+import com.websudos.phantom.dsl._
+import domain.sites.Site
+import services.Service
+import services.sites.impl.SitesServiceImpl
+
+import scala.concurrent.Future
+
+/**
+  * Created by Quest on 2016/10/25.
+  */
+trait SitesService {
+  def save(site: Site): Future[ResultSet]
+  def getSiteById(siteId:String,name:String): Future[Option[Site]]
+  def getAllSites:Future[Seq[Site]]
+  def updateSite(site: Site): Future[ResultSet]
+}
+object SitesService extends Service{
+  def apply():SitesService = new SitesServiceImpl
+}
