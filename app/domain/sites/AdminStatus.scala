257ef5395cba10e1af66f0653cccb9a6fb1fe30f
Boniface Kabaso <boniface@kabaso.com>
2016-11-30 22:16:56 +0200

    Added Controller Routes for Site and Changed the Test Cases for Repository, Services and COntroller

--- app/domain/sites/AdminStatus.scala
+++ app/domain/sites/AdminStatus.scala
@@ -5,9 +5,9 @@ import play.api.libs.json.Json
 /**
   * Created by hashcode on 2016/09/21.
   */
-case class AdminStatus(
-                        adminStatusId:String,
-                        name:String)
-object AdminStatus{
-implicit val administatusFmt =Json.format[AdminStatus]
+case class AdminStatus(adminStatusId: String,
+                       name: String)
+
+object AdminStatus {
+  implicit val administatusFmt = Json.format[AdminStatus]
 }
--- app/domain/sites/Administrators.scala
+++ app/domain/sites/Administrators.scala
@@ -5,8 +5,9 @@ import play.api.libs.json.Json
 /**
   * Created by hashcode on 2016/09/21.
   */
-case class Administrators(siteId:String,emailId:String)
-object Administrators{
+case class Administrators(siteId: String, emailId: String)
 
-  implicit val administratorsFmt =Json.format[Administrators]
+object Administrators {
+
+  implicit val administratorsFmt = Json.format[Administrators]
 }
