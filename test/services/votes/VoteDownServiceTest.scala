6af4a6a89fa497c690cbb141a4762ce39d4ecc3f
mogamatnoorjacobs <mogamatjacobs@gmail.com>
2016-12-07 21:34:31 +0200

    Votes Controller,Router and Services Tests

---
+++ test/services/votes/VoteDownServiceTest.scala
@@ -0,0 +1,32 @@
+package services.votes
+import domain.votes.VoteDown
+
+import org.scalatest.FunSuite
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+/**
+  * Created by fatimam on 07/12/2016.
+  */
+class VoteDownServiceTest extends FunSuite
+{
+  test("testSave"){
+    val voteDown = VoteDown("1","1","156.326.232",1)
+    val result = Await.result(VoteDownService.apply().saveOrUpdate(voteDown), 2minutes)
+
+    assert(result.isExhausted)
+  }
+
+  test("testGetVoteDownId"){
+    val result = Await.result(VoteDownService.apply().get("1"),2 minutes)
+    assert(result === "1")
+  }
+
+
+  test("testGetAll") {
+    val result = Await.result(VoteDownService.apply().getAll, 2.minutes)
+
+    assert(result === "test")
+  }
+
+}
---
+++ test/services/votes/VoteUpServiceTest.scala
@@ -0,0 +1,32 @@
+package services.votes
+
+import domain.votes.VoteUp
+import org.scalatest.FunSuite
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+/**
+  * Created by fatimam on 07/12/2016.
+  */
+class VoteUpServiceTest extends FunSuite
+{
+  test("testSave"){
+    val voteUp = VoteUp("1","1","156.326.232",1)
+    val result = Await.result(VoteUpService.apply().saveOrUpdate(voteUp), 2minutes)
+
+    assert(result.isExhausted)
+  }
+
+  test("testGetVoteDownId"){
+    val result = Await.result(VoteUpService.apply().get("1"),2 minutes)
+    assert(result === "1")
+  }
+
+
+  test("testGetAll") {
+    val result = Await.result(VoteUpService.apply().getAll, 2.minutes)
+
+    assert(result === "test")
+  }
+
+}
