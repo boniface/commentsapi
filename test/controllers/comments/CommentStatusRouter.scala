b776eae0c5e46d010e9e9e49203569869d079d39
202136272 <bmabulu@webmail.co.za>
2016-12-16 10:53:36 +0200

    redone conrtollers

--- test/controllers/comments/CommentStatusRouter.scala
+++ test/controllers/comments/CommentStatusRouter.scala
@@ -1,8 +1,19 @@
 package controllers.comments
+import play.api.routing.Router._
+import play.api.routing.sird._
+import javax.inject.Inject
+import play.api.routing.SimpleRouter
 
 /**
   * Created by Bonga on 12/2/2016.
   */
-class CommentStatusRouter {
+class CommentStatusRouter  @Inject() (commentStatus: CommentStatusController) extends SimpleRouter{
 
+
+  override def routes: Routes = {
+    case GET(p"/get/commentStatus") =>
+      commentStatus.getCommentStatus(subjectId="100")
+    case POST(p"/create") =>
+      commentStatus.createOrUpdate
+  }
 }
--- test/controllers/comments/ResponseStatusRouter.scala
+++ test/controllers/comments/ResponseStatusRouter.scala
@@ -1,8 +1,23 @@
 package controllers.comments
+import play.api.routing.Router._
+import play.api.routing.sird._
+import javax.inject.Inject
+import play.api.routing.SimpleRouter
+
 
 /**
   * Created by Bonga on 12/2/2016.
   */
-class ResponseStatusRouter {
+class ResponseStatusRouter @Inject() (responseStatus: ResponseStatusController) extends SimpleRouter{
+
+
+  override def routes: Routes = {
+    case GET(p"/get/responseStatus") =>
+      responseStatus.getResponseStatus(subjectId="100")
+    case GET(p"/get/all") =>
+      responseStatus.getAllResponseStatus
+    case POST(p"/create") =>
+      responseStatus.createOrUpdate
+  }
 
 }
